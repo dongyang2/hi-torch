2538ee92daa45d2b8871d47d96f4879229547d1a
dongyang2 <dongyangzhao@outlook.com>
2019-01-13 17:13:45 +0800

    update 第五节

--- hi/5.DATA_LOADING_PROCESSING.py
+++ hi/5.DATA_LOADING_PROCESSING.py
@@ -12,6 +12,21 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
+def show_landmarks(img, landmark):
+    # plt.figure()
+    plt.imshow(img)
+    plt.scatter(landmark[:, 0], landmark[:, 1], s=10, marker='.', c='r')
+    plt.pause(0.001)
+
+
+# def get_picture_path(name):
+#     now_path = os.getcwd()
+#     pro_path = '/'.join(now_path.split('\\')[:-1])
+#     return os.path.join(pro_path+'/data/faces/', name)
+
+
+class FaceLandmarkDataset(Dataset):
+    """为我们的数据集单独定制一个类，继承torch里的Dataset类"""
 if __name__ == '__main__':
 
     plt.ion()
@@ -21,10 +36,16 @@ if __name__ == '__main__':
 
     n = 65  # 选第65个图片
     img_name = landmarks_frame.iloc[n, 0]
-    print(img_name)
+    # print(img_name)
     landmarks = landmarks_frame.iloc[n, 1:].as_matrix()
     landmarks = landmarks.astype('float').reshape(-1, 2)
 
     print('image name : {}'.format(img_name))
     print('landmark shape: {}'.format(landmarks.shape))
     print('First 4 landmarks : {}'.format(landmarks[:4]))
+
+    img_path = os.path.join('../data/faces/', img_name)
+    image1 = io.imread(img_path)
+    # print(image1)
+    show_landmarks(image1, landmarks)
+    # io.imshow(image1)
